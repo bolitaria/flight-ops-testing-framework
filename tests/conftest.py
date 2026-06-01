6f020300cc036a13c5859f2354a43890abaa6cfd
bolitaria <bolitariaa@gmail.com>
2026-06-01 18:08:12 +0200

    Fix CI: add PYTHONPATH to conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,12 +4,11 @@ import pytest
 import subprocess
 import time
 
-# Añadir la raíz del proyecto al PYTHONPATH para que 'tests' sea un módulo importable
+# Make the project root discoverable so that `tests` is importable
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 @pytest.fixture(scope="session", autouse=True)
 def docker_services():
-    """Levanta Docker automáticamente para todas las pruebas."""
     subprocess.run(["docker", "compose", "up", "-d"], check=True)
     time.sleep(5)
     yield
